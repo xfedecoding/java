1cd14704cdb40a2b63a3cc815179d8cd9b073abe
ingfe <ingfe@Federico.station>
2023-02-04 20:16:44 +0100

    first commit

---
+++ prova1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ingfe
+ *
+ */
+module prova1 {
+}
---
+++ prova1/src/prova1/my_program.java
@@ -0,0 +1,10 @@
+package prova1;
+
+public class my_program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
